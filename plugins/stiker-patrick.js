ae6d4a869d6aec8de794c2bf297de0d4a87c89a7
Andy-GG3 <118534297+Andy-GG3@users.noreply.github.com>
2024-05-19 13:24:24 +0700

    Add files via upload

---
+++ plugins/stiker-patrick.js
@@ -0,0 +1,31 @@
+let fetch = require('node-fetch')
+const { Sticker } = require('wa-sticker-formatter')
+
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+    let images = `https://api.botcahx.eu.org/api/sticker/patrick?apikey=${apichan}`
+   try {
+   let stiker = await createSticker(images, false, global.packname, global.author)
+    if (stiker) return conn.sendFile(m.chat, stiker, 'Quotly.webp', '', m)
+} catch (e) {
+ m.reply('Erorr: Tidak dapat menemukan media')
+ }
+}
+
+handler.menu = ['spatrick']
+handler.tags = ['stiker']
+handler.command = /^(spetrick|stikerpatrick|patrick)$/i
+handler.premium = false
+handler.limit = true
+module.exports = handler
+async function createSticker(img, url, packName, authorName, quality) {
+	let stickerMetadata = {
+		type: 'full',
+		pack: global.packname,
+		author: global.author,
+		quality
+	}
+	return (new Sticker(img ? img : url, stickerMetadata)).toBuffer()
+}
+function pickRandom(list) {
+    return list[Math.floor(list.length * Math.random())]
+}
